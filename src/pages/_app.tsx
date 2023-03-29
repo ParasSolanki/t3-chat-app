00983d28d85157379ab296a5e3941c77373308fc
parasSolanki <parassolanki151@gmail.com>
2023-03-29 21:48:34 +0530

    add: nprogress

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,10 +1,17 @@
 import { type AppType } from "next/app";
 import { type Session } from "next-auth";
 import { SessionProvider } from "next-auth/react";
+import Router from "next/router";
+import NProgress from "nprogress";
 
 import { api } from "~/utils/api";
 
 import "~/styles/globals.css";
+import "~/styles/nprogress.css";
+
+Router.events.on("routeChangeStart", () => NProgress.start());
+Router.events.on("routeChangeComplete", () => NProgress.done());
+Router.events.on("routeChangeError", () => NProgress.done());
 
 const MyApp: AppType<{ session: Session | null }> = ({
   Component,
