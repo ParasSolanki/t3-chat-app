b0406b7e39c54522821365aa16edbcfa5a9309e4
parasSolanki <parassolanki151@gmail.com>
2023-04-13 19:30:10 +0530

    fix: on hover append icon overflow issue on input

--- src/components/forms/FormField.tsx
+++ src/components/forms/FormField.tsx
@@ -4,6 +4,7 @@ import {
   type UseFormReturn,
   useFormContext,
 } from "react-hook-form";
+import { cn } from "~/lib/utils";
 
 export interface UseFormFieldProps {
   name: string;
@@ -39,7 +40,15 @@ const FormField = ({ label, name, id, renderItem }: FormFieldProps) => {
       <label htmlFor={id} className="font-medium text-zinc-300">
         {label}
       </label>
-      <div className="relative mt-1 overflow-hidden rounded-md">
+      <div
+        className={cn(
+          "relative mt-1 block w-full overflow-hidden rounded-md border-2 border-neutral-700 bg-neutral-900 text-base text-slate-200 shadow-sm focus:bg-neutral-800/90 focus:outline-none focus:ring-0",
+          {
+            "focus-within:border-red-500": state.invalid,
+            "focus-within:border-orange-400": !state.invalid,
+          }
+        )}
+      >
         {renderItem(state)}
       </div>
       {state.error ? (
--- src/components/forms/Input.tsx
+++ src/components/forms/Input.tsx
@@ -24,14 +24,7 @@ const Input = forwardRef<HTMLInputElement, Props>((props, ref) => {
             ref={ref}
             {...otherProps}
             aria-invalid={state.invalid}
-            className={cn(
-              "block w-full rounded-md border-2 border-neutral-700 bg-neutral-900 px-3 py-2 text-base text-slate-200 shadow-sm focus:bg-neutral-800/90 focus:outline-none focus:ring-0",
-              {
-                "focus-within:border-red-500": state.invalid,
-                "focus-within:border-orange-400": !state.invalid,
-              },
-              className
-            )}
+            className={cn("w-full border-none bg-inherit px-3 py-2", className)}
           />
           {props?.appendicon && props.appendicon}
         </>
